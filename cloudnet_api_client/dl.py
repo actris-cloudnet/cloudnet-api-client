15567fa1fbd038d35635f562492eac9b677f991e
Tuomas Siipola <tuomas.siipola@fmi.fi>
2025-04-01 10:32:21 +0300

    Add download progress bar

--- cloudnet_api_client/dl.py
+++ cloudnet_api_client/dl.py
@@ -5,6 +5,8 @@ from os import PathLike
 from pathlib import Path
 
 import aiohttp
+from tqdm import tqdm
+from tqdm.asyncio import tqdm_asyncio
 
 from cloudnet_api_client import utils
 from cloudnet_api_client.containers import ProductMetadata, RawMetadata
@@ -13,14 +15,23 @@ MetadataList = list[ProductMetadata] | list[RawMetadata]
 
 
 def download(
-    metadata: MetadataList, output_directory: str | PathLike, concurrency_limit: int = 5
+    metadata: MetadataList,
+    output_directory: str | PathLike,
+    concurrency_limit: int = 5,
+    progress: bool | None = None,
 ) -> None:
+    disable_progress = not progress if progress is not None else None
     os.makedirs(output_directory, exist_ok=True)
-    asyncio.run(_download_files(metadata, output_directory, concurrency_limit))
+    asyncio.run(
+        _download_files(metadata, output_directory, concurrency_limit, disable_progress)
+    )
 
 
 async def _download_files(
-    metadata: MetadataList, output_path: str | PathLike, concurrency_limit: int
+    metadata: MetadataList,
+    output_path: str | PathLike,
+    concurrency_limit: int,
+    disable_progress: bool | None,
 ) -> None:
     semaphore = asyncio.Semaphore(concurrency_limit)
     async with aiohttp.ClientSession() as session:
@@ -32,11 +43,13 @@ async def _download_files(
                 continue
             task = asyncio.create_task(
                 _download_file_with_retries(
-                    session, meta.download_url, destination, semaphore
+                    session, meta.download_url, destination, semaphore, disable_progress
                 )
             )
             tasks.append(task)
-        await asyncio.gather(*tasks)
+        await tqdm_asyncio.gather(
+            *tasks, desc="Completed files", disable=disable_progress
+        )
 
 
 async def _download_file_with_retries(
@@ -44,12 +57,13 @@ async def _download_file_with_retries(
     url: str,
     destination: Path,
     semaphore: asyncio.Semaphore,
+    disable_progress: bool | None,
     max_retries: int = 3,
 ) -> None:
     """Attempt to download a file, retrying up to max_retries times if needed."""
     for attempt in range(1, max_retries + 1):
         try:
-            await _download_file(session, url, destination, semaphore)
+            await _download_file(session, url, destination, semaphore, disable_progress)
             return
         except Exception as e:
             logging.warning(f"Attempt {attempt} failed for {url}: {e}")
@@ -65,16 +79,28 @@ async def _download_file(
     url: str,
     destination: Path,
     semaphore: asyncio.Semaphore,
+    disable_progress: bool | None,
 ) -> None:
     async with semaphore:
         async with session.get(url) as response:
             response.raise_for_status()
-            with destination.open("wb") as file_out:
+            with (
+                destination.open("wb") as file_out,
+                tqdm(
+                    desc=destination.name,
+                    total=response.content_length,
+                    unit="iB",
+                    unit_scale=True,
+                    unit_divisor=1024,
+                    disable=disable_progress,
+                ) as bar,
+            ):
                 while True:
                     chunk = await response.content.read(8192)
                     if not chunk:
                         break
                     file_out.write(chunk)
+                    bar.update(len(chunk))
         logging.info(f"Downloaded: {destination}")
 
 
