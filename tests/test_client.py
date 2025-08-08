04a0e46e2efdfaca1a77dc0065fcb4494e85a3ba
Simo Tukiainen <simo.tukiainen@fmi.fi>
2025-08-08 15:52:11 +0300

    Add tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -39,19 +39,76 @@ class TestFixtures:
 class TestWithRawFiles:
     def setup_method(self):
         self.client = APIClient(base_url=f"{BACKEND_URL}/api/")
-        _submit_file(
-            "20250801_Magurele_CHM170137_000.nc",
-            "bucharest",
-            "chm15k",
-            "2025-08-01",
-            "https://hdl.handle.net/21.12132/3.c60c931fac9d43f0",
-        )
+        metadata_list = [
+            (
+                "20250801_Magurele_CHM170137_000.nc",
+                "bucharest",
+                "chm15k",
+                "2025-08-01",
+                "https://hdl.handle.net/21.12132/3.c60c931fac9d43f0",
+            ),
+            (
+                "20250808_Granada_CHM170119_0045_000.nc",
+                "granada",
+                "chm15k",
+                "2025-08-08",
+                "https://hdl.handle.net/21.12132/3.77a75f3b32294855",
+            ),
+            (
+                "20250803_JOYCE_WST_01m.dat",
+                "juelich",
+                "weather-station",
+                "2025-08-01",
+                "https://hdl.handle.net/21.12132/3.726b3b29de1949cc",
+            ),
+        ]
 
-    def test_raw_metadata(self):
-        raw_metadata = self.client.raw_metadata(site_id="bucharest", date="2025-08-01")
-        assert isinstance(raw_metadata, list)
-        assert len(raw_metadata) == 1
-        assert isinstance(raw_metadata[0], RawMetadata)
+        for item in metadata_list:
+            _submit_file(*item)
+
+    def test_raw_metadata_1(self):
+        meta = self.client.raw_metadata(site_id="bucharest", date="2025-08-01")
+        assert isinstance(meta, list)
+        assert len(meta) == 1
+        assert isinstance(meta[0], RawMetadata)
+
+    def test_raw_metadata_2(self):
+        meta = self.client.raw_metadata(date="2025-08-08")
+        assert len(meta) == 1
+
+    def test_raw_metadata_3(self):
+        pid = "https://hdl.handle.net/21.12132/3.77a75f3b32294855"
+        meta = self.client.raw_metadata(instrument_pid=pid)
+        assert len(meta) == 1
+
+    def test_raw_metadata_4(self):
+        pid = "https://hdl.handle.net/21.12132/3.77a75f3b32294855"
+        meta = self.client.raw_metadata(instrument_pid=pid, date="2022-01-01")
+        assert len(meta) == 0
+
+    def test_raw_metadata_5(self):
+        meta = self.client.raw_metadata(date_from="2025-08-01")
+        assert len(meta) == 3
+
+    def test_raw_metadata_6(self):
+        meta = self.client.raw_metadata(date_from="2025-08-01", date_to="2025-08-08")
+        assert len(meta) == 3
+
+    def test_raw_metadata_7(self):
+        meta = self.client.raw_metadata(date_from="2025-08-01", date_to="2025-08-07")
+        assert len(meta) == 2
+
+    def test_raw_metadata_8(self):
+        meta = self.client.raw_metadata(filename_prefix="20250801")
+        assert len(meta) == 1
+
+    def test_raw_metadata_9(self):
+        meta = self.client.raw_metadata(filename_suffix="000.nc")
+        assert len(meta) == 2
+
+    def test_raw_metadata_10(self):
+        meta = self.client.raw_metadata(instrument_id="weather-station")
+        assert len(meta) == 1
 
 
 def _submit_file(filename: str, site: str, instrument: str, date: str, pid: str):
@@ -71,8 +128,12 @@ def _submit_file(filename: str, site: str, instrument: str, date: str, pid: str)
     }
 
     res = requests.post(f"{BACKEND_URL}/upload/metadata/", json=metadata, auth=auth)
-    res.raise_for_status()
-
-    with open(file_path, "rb") as f:
-        res = requests.put(f"{BACKEND_URL}/upload/data/{checksum}", data=f, auth=auth)
+    if res.status_code not in (200, 409):
         res.raise_for_status()
+
+    if res.status_code == 200:
+        with open(file_path, "rb") as f:
+            res = requests.put(
+                f"{BACKEND_URL}/upload/data/{checksum}", data=f, auth=auth
+            )
+            res.raise_for_status()
